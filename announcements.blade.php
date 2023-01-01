43429f664626d8fea9172cbaa7271ab34befafd1
rumanhassan25 <88835371+rumanhassan25@users.noreply.github.com>
2023-01-01 08:06:19 +0300

    Add files via upload

---
+++ announcements.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Announcements</title>
+   
+</head>
+
+<body>
+    <div class="container mt-2">
+        <div class="row">
+            <div class="col-lg-12 margin-tb">
+                <div class="pull-left mb-2">
+                    <h2>Create Announcements</h2>
+                </div>
+                <div class="pull-right">
+                    <a class="btn btn-primary" href="{{ route('lecturer2') }}"> Back</a>
+                </div>
+            </div>
+        </div>
+        @if(session('status'))
+        <div class="alert alert-success mb-1 mt-1">
+            {{ session('status') }}
+        </div>
+        @endif
+        <form action="/announcements" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Title</strong><br>
+                        <input type="text" name="title" class="form-control" placeholder="...">
+                        @error('title')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                        @enderror
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Body</strong><br>
+                        <input type="text" name="body" class="form-control" placeholder="...">
+                        @error('body')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                        @enderror
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Date</strong><br>
+                        <input type="date" name="date" class="form-control" placeholder="dd/mm/yy">
+                        @error('date')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                        @enderror
+                    </div>
+                </div>
+                
+                <button type="submit" class="btn btn-primary ml-3">Submit</button>
+            </div>
+        </form>
+    </div>
+</body>
+
+</html>
---
+++ create.blade.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Add StudentForm - Laravel 9 CRUD</title>
+   
+</head>
+
+<body>
+    <div class="container mt-2">
+        <div class="row">
+            <div class="col-lg-12 margin-tb">
+                <div class="pull-left mb-2">
+                    <h2>Add Student</h2>
+                </div>
+                <div class="pull-right">
+                    <a class="btn btn-primary" href="{{ route('lecturer2') }}"> Back</a>
+                </div>
+            </div>
+        </div>
+        @if(session('status'))
+        <div class="alert alert-success mb-1 mt-1">
+            {{ session('status') }}
+        </div>
+        @endif
+        <form action="/create" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Admission Number:</strong>
+                        <input type="number" name="admission_no" class="form-control" placeholder="Admission Number">
+                        @error('admission_no')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                        @enderror
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>First Name:</strong>
+                        <input type="text" name="fname" class="form-control" placeholder="First Name">
+                        @error('fname')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                        @enderror
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Last Name:</strong>
+                        <input type="text" name="lname" class="form-control" placeholder="last Name">
+                        @error('lname')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                        @enderror
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Course:</strong>
+                        <input type="text" name="course" class="form-control" placeholder="Course">
+                        @error('address')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                        @enderror
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-primary ml-3">Submit</button>
+            </div>
+        </form>
+    </div>
+</body>
+
+</html>
