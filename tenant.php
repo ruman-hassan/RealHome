0020cebfa9f2110738363f4060e8fb0fc8c4a92b
ruman-hassan <88835371+ruman-hassan@users.noreply.github.com>
2023-11-25 23:47:23 +0300

    Added the tenant landing page

---
+++ tenant.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+	<title>Tenant Page</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<body>
+
+		<img src="./uploadimages/logo.png" alt="Logo" width="200" height="200">
+		<div>
+			<a href="homepage.php"><img src="https://cdn-icons-png.flaticon.com/128/1946/1946488.png" height="30" width="30"></a></br>
+			<a href="user2.php"><img src="https://cdn-icons-png.flaticon.com/128/6380/6380189.png" height="30" width="30"></a></br>
+			
+			
+		</div>
+
+		<h3>Welcome Tenant</h3>
+		<h3>Apartment Booked</h3>
+		<table>
+			<tr>
+				<th>Apartment ID</th>
+				<th>Apartment Location</th>
+				<th>Apartment Floor</th>
+				<th>Apartment Image</th>
+				<th>No of Rooms</th>
+				
+			</tr>
+			<tr>
+				<td>row 1</td>
+				<td>row 1</td>
+				<td>row 1</td>
+				<td>row 1</td>
+				<td>row 1</td>
+				
+			</tr>
+		</table>
+
+		<h3>Apartment Approved</h3>
+		<table>
+			<tr>
+				<th>Apartment ID</th>
+				<th>Apartment Location</th>
+				<th>Apartment Floor</th>
+				<th>Apartment Image</th>
+				<th>No of Rooms</th>
+				
+			</tr>
+			<tr>
+				<td>row 1</td>
+				<td>row 1</td>
+				<td>row 1</td>
+				<td>row 1</td>
+				<td>row 1</td>
+				
+			</tr>
+		</table>
+
+			
+			 	
+		<footer>
+      
+
+
+
+<li class="list1">
+		<ul><u>Contact Info</u></ul>
+		<ul>Real Homes,Pioneer House,</ul>
+		<ul>Kimathi Street, Shop 14</ul>
+		<ul>P.O. Box 71085</ul>
+		<ul>Tel. No.: +254712345678</ul>
+		<ul>realhomes@gmail.com</ul>
+	</li>
+<p><img src="https://cdn-icons-png.flaticon.com/128/1294/1294391.png" height="10" width="10">Real Homes,2022</p>
+	
+</footer>
+			 	
+			 </div>   
+	</body>
+</html>
